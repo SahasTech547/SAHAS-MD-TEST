3703e7ac89619b980013ac7710170144ec36e747
Sahas Tech <sahastech547@gmail.com>
2024-10-25 18:47:55 +0530

    Update 01. Main Plugin.js

--- Plugin/01. Main Plugin.js
+++ Plugin/01. Main Plugin.js
@@ -1118,3 +1118,27 @@ _*🌟 Reply with the Number you want to select*_
         reply('An error occurred while processing your request.');
     }
 });
+
+
+//=================== ping command =======================
+
+cmd({
+    pattern: "ping",
+    react: "⚡",
+    alias: ["speed"],
+    desc: "Check bot\'s ping",
+    category: "main",
+    use: '.ping',
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+var inital = new Date().getTime();
+let ping = await conn.sendMessage(from , { text: '```Pinging To index.js!!!```'  }, { quoted: mek } )
+var final = new Date().getTime();
+return await conn.edit(ping, '*Pong*\n *' + (final - inital) + ' ms* ' )
+} catch (e) {
+reply(`${e}`)
+console.log(e)
+}
+})
