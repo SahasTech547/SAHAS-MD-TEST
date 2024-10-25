3425b3ef0d9e72c364bcd36c731532df6eaee5c6
Sahas Tech <sahastech547@gmail.com>
2024-10-25 15:54:49 +0530

    Update apk.js

--- Plugin/apk.js
+++ Plugin/apk.js
@@ -32,7 +32,7 @@ async (conn, mek, m, { from, quoted, body, q, reply }) => {
         
         await conn.sendMessage(
             from,
-            { document: buff, caption: `> *©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜ*`, mimetype: "application/vnd.android.package-archive", filename: `${appInfo.appname}.apk` },
+            { document: buff, caption: `> *ꜱᴀʜᴀꜱ • ᴍᴜʟᴛɪ ᴅᴇᴠɪᴄᴇ-ᴡᴀ-ʙᴏᴛ ㋛*`, mimetype: "application/vnd.android.package-archive", filename: `${appInfo.appname}.apk` },
             { quoted: mek }
         );
         
